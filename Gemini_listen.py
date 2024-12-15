45521224760ecf83d56c61ea4c746e363cf2c692
A1exan10er <yangtianqwer@gmail.com>
2024-12-15 22:39:29 +0100

    Refactor Gemini class to comment out unused text-to-speech functionality and enhance user input handling

--- Gemini_listen.py
+++ Gemini_listen.py
@@ -10,14 +10,15 @@
 
 import google.generativeai as genai
 import os # for reading api key
-import pyttsx3
+# import pyttsx3
 
 class Gemini:
     def __init__(self, api_key_file_path):
         self.api_key_file_path = api_key_file_path
         self.initialize_gemini(self.api_key_file_path)
         self.model = genai.GenerativeModel("gemini-1.5-flash-8b")
-        self.engine = pyttsx3.init()
+        # self.engine = pyttsx3.init()
+        self.response = None # response from Gemini, to store the response from the model
 
     def initialize_gemini(self, api_key_file):
         try:
@@ -34,21 +35,26 @@ class Gemini:
             print(f"Error initializing Gemini API: {e}")
             exit(1) # exit the program, return 1 to indicate error, 0 for success (any non-zero value indicates error)
 
-    def text_input_mode(self):
+    def text_input_mode(self): # Get user input from text and write the conversation to a file
         while True:
-            user_input = input("You (enter 'exit' to quit): ")
-            if user_input.lower() == "exit":
+            user_exit_keywords = ["exit", "q", "quit"]
+            user_input = input(f"You (enter {user_exit_keywords} to quit): ")
+            if user_input.lower() in user_exit_keywords:
                 break
             response = self.model.generate_content(user_input)
             print(f"Gemini: {response.text}")
-            self.text_to_speech(response.text)
+            with open("response.txt", "a") as file:
+                file.write(f"User: {user_input}\n")
+                file.write(f"Gemini: {response.text}\n")
+            # print(response.usage_metadata) # Show token usage metadata
+            # self.text_to_speech(response.text)
 
     def voice_input_mode(self):
         pass
     
-    def text_to_speech(self, text): # TODO: get the voice to sound more human-like
-        self.engine.say(text)
-        self.engine.runAndWait()
+    # def text_to_speech(self, text): # TODO: get the voice to sound more human-like
+    #     self.engine.say(text)
+    #     self.engine.runAndWait()
 
 if __name__ == "__main__":
     api_key_file_path = "/home/pi/Projects/Gemini_API/api_key.txt"
